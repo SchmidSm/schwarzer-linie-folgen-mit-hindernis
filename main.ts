f02c74ee2eeaaa19335c1a6551b44b928df8316a
SchmidSm <106902811+SchmidSm@users.noreply.github.com>
2023-03-27 18:37:33 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,31 @@
+input.onButtonPressed(Button.A, function () {
+    maqueen.motorStop(maqueen.Motors.All)
+})
 basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 152)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 152)
+        basic.pause(200)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 56)
+        basic.pause(800)
+        for (let index = 0; index < 2; index++) {
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 152)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 152)
+            basic.pause(200)
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 56)
+            basic.pause(800)
+        }
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 152)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 152)
+        basic.pause(200)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    }
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 29)
     } else {
@@ -24,20 +51,5 @@ basic.forever(function () {
                 }
             }
         }
-    }
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
-        maqueen.motorStop(maqueen.Motors.All)
-        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 152)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-        basic.pause(200)
-        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 148)
-        basic.pause(200)
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 150)
-        basic.pause(200)
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
     }
 })
