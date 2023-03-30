1729b94d3e44e4ddc1c2908cb4f5d4258bc53c69
SchmidSm <106902811+SchmidSm@users.noreply.github.com>
2023-03-30 10:29:39 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     maqueen.motorStop(maqueen.Motors.All)
 })
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 5 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
         maqueen.motorStop(maqueen.Motors.All)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 152)
